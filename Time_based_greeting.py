17a2b8f5fae9d634e5b86e3eaf6f8cea2101e708
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2024-11-14 12:15:25 +0500

    Update Time_based_greeting.py

--- Time_based_greeting.py
+++ Time_based_greeting.py
@@ -1,11 +1,3 @@
-# Time-based Greeting Program
-# This program:
-# 1. Gets and shows the current time in hours, minutes, and seconds.
-# 2. Checks the current hour to decide what greeting to show:
-#    - Before 12 PM: "GOOD MORNING"
-#    - Between 12 PM and 6 PM: "GOOD NOON"
-#    - After 6 PM: "GOOD NIGHT"
-
 import time
 c_time=time.strftime('%H:%M:%S')
 print()
