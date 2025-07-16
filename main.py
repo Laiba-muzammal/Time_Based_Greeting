9f2f4d1d1807cdd4f550b968aad525ec71fc8cbc
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2025-07-16 23:22:13 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from greeter import get_current_time, get_greeting
+
+current_time = get_current_time()
+greeting = get_greeting()
+
+print()
+print(f"{greeting}\nThe current time is: {current_time}")
